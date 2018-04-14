eee14cf4562988e616b2e1243745bce155a77354
Patrick Leisinger <patrick.leisinger@gmail.com>
2018-04-14 17:51:26 +0200

    Changed Text-Transform for Status, Quote and Link Title

--- page-log.php
+++ page-log.php
@@ -10,6 +10,10 @@ get_header(); ?>
 				</header>
 				<hr>
 				<div class="page-content">
+					<h2>April 14, 2018</h2>
+					<ul class="fa-ul">
+						<li><span class="fa-li"><i class="fal fa-wrench"></i></span><strong>Changed</strong> text-transform for Status, Quote and Link title</li>
+					</ul>						
 					<h2>April 13, 2018</h2>
 					<ul class="fa-ul">
 						<li><span class="fa-li"><i class="fal fa-plus-circle"></i></span><strong>Added</strong> new SoaPatrickFive Theme</li>
