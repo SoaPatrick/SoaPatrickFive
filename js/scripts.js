7ff035b4c05ffbaa921e85646705e0ef789774e6
Patrick Leisinger <patrick.leisinger@gmail.com>
2018-08-04 10:41:19 +0200

    fixes to search input and behaviour
    - changed position of mobile navigation
    - changed blockquote post styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ window.onload = function(){
 	
 	// open/close search
 	document.getElementById('toggle-search-collapse').onclick = function() {
-	    document.getElementById('search-collapse').classList.toggle('open');
-	    document.getElementById('search-collapse--input').focus();
+    document.getElementById('search-collapse').classList.toggle('open');
+    document.getElementById('search-collapse--input').focus();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera    
 		document.getElementById('search-collapse--input').value = "";	    
 	};	
 };
