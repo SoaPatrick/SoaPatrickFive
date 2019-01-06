663e1d9fc11d499b1f9178c8e8d02766171a2e8b
SoaPatrick <patrick.leisinger@gmail.com>
2019-01-06 20:05:54 +0100

    update fontawesome to 5.6.3
    - fix lincence for myfonts in head

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 	<!--
 	/**
 	 * @license
-	 * MyFonts Webfont Build ID 3558193, 2018-04-13T11:11:57-0400
+	 * MyFonts Webfont Build ID 3694353, 2019-01-06T13:57:31-0500
 	 * 
 	 * The fonts listed in this notice are subject to the End User License
 	 * Agreement(s) entered into by the website owner. All other parties are 
@@ -39,15 +39,16 @@
 	 * 
 	 * You may obtain a valid license at the URLs below.
 	 * 
-	 * Webfont: QanelasSoft-ExtraBold by Radomir Tinkov
-	 * URL: https://www.myfonts.com/fonts/radomir-tinkov/qanelas-soft/extra-bold/
-	 * Copyright: Copyright &#x00A9; 2015 by Radomir Tinkov. All rights reserved.
+	 * Webfont: AmsiProNarw-Black by Stawix
+	 * URL: https://www.myfonts.com/fonts/stawix/amsi-pro/narrow-black/
+	 * Copyright: &#x00A9; 2012, 2013, 2014, 2015, 2016 by Stawix Ruecha &amp; Foundry. All
+	 * rights reserved.
 	 * Licensed pageviews: 10,000
 	 * 
 	 * 
-	 * License: https://www.myfonts.com/viewlicense?type=web&buildid=3558193
+	 * License: https://www.myfonts.com/viewlicense?type=web&buildid=3694353
 	 * 
-	 * © 2018 MyFonts Inc
+	 * © 2019 MyFonts Inc
 	*/
 	
 	-->	
